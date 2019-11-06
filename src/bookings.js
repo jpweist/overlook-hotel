bc9c2dfdb106d979d69182b099bbabb0b71c92fa
Jim Weist <jp_weist@yahoo.com>
2019-11-05 17:51:19 -0700

    removed most comments

--- src/bookings.js
+++ src/bookings.js
@@ -21,30 +21,3 @@ export default Bookings;
 // "date":"2019/11/18",
 // "roomNumber":5,
 // "roomServiceCharges":[]}
-
-
-
-
-
-
-// class Customer {
-//   constructor(object) {
-//     this.id = object.id;
-//     this.name = object.name;
-//   }
-//   seeAvailableRoomsDate(date) {
-//     //see all the available rooms by date
-//   }
-//   allBookings() {
-//     //return all the past present and upcoming bookings
-//   }
-//   totalSpent() {
-//     //total ammount the the customer has spent
-//   }
-//   bookRoom() {
-//     //book selected room
-//   }
-//
-// }
-//
-// export default Customer;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,14 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you import jQuery into a JS file if you use jQuery in that file
-import $ from 'jquery';
-
-// import Manger from 'Manger'
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 import Manager from '../src/Manager';
 import Customer from '../src/Customer';
 import Bookings from '../src/Bookings';
 import Rooms from '../src/Rooms';
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png';
 $( document ).ready(function() {
 let manager, customer, bookings;
 let rooms;
 let roomData, bookingsData;
 let getCounter = 0;
-
-// fetch request to: (make DRY like last project)
-// https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users
-// https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms
-// https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings
 
 function getData(ext, callback) {
   fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/' + ext)
@@ -49,20 +35,13 @@ function makeClasses() {
   })
 }
 
-
-
-
-
 $('.login-button').on( "click", function() { // login manager or customer
 
   let userName = $('.user-login').val();
   let userpassword = $('.password').val();
 
   if (userName === 'manager' && userpassword === 'overlook2019') {
-    // let manager1 = new Manager(userName);
-    // let bookings1 = new Bookings(bookingsData);
-    // getHelper('rooms/rooms', (roomData));
-    // bookings.seeAvailableRoomsDate('2019/11/06');
+
     window.location = 'manager.html';
   }
    if (userName === 'customer' && userpassword === 'overlook2019') {
@@ -74,41 +53,4 @@ $('.login-button').on( "click", function() { // login manager or customer
     console.log('failed login')
   }
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var ctx = $('#myChart').getContext('2d');
-// var chart = new Chart(ctx, {
-//     // The type of chart we want to create
-//     type: 'line',
-//
-//     // The data for our dataset
-//     data: {
-//         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-//         datasets: [{
-//             label: 'My First dataset',
-//             backgroundColor: 'rgb(255, 99, 132)',
-//             borderColor: 'rgb(255, 99, 132)',
-//             data: [0, 10, 5, 2, 20, 30, 45]
-//         }]
-//     },
-//
-//     // Configuration options go here
-//     options: {}
-// });
-
-
-
-
-
 }); // end jQuery
